771591e8a9bd949251fc5115b7978cae6b92d6d5
megz15 <meghraj.g16@gmail.com>
2024-06-03 18:42:18 +0530

    feat: add min, max correlations

--- reg_ext_enso_corr_minmax.py
+++ reg_ext_enso_corr_minmax.py
@@ -37,7 +37,19 @@ month_list = [
     'December'
 ]
 
-corr_values = []
+corr_values, minima_corr_values, minima_below_bfl_corr_values, maxima_corr_values, maxima_above_bfl_corr_values = [], [], [], [], []
+
+max_template = {
+    "value": 0,
+    "month": '',
+    "sector": '',
+}
+
+max_corr = max_template.copy()
+max_minima_corr = max_template.copy()
+max_minima_below_bfl_corr = max_template.copy()
+max_maxima_corr = max_template.copy()
+max_maxima_above_bfl_corr = max_template.copy()
 
 # Sea Ice Index Data
 excel_path = "./S_Sea_Ice_Index_Regional_Monthly_Data_G02135_v3.0.xlsx"
@@ -58,8 +70,20 @@ for sheet in [x for x in df_sea_ice.keys() if "Extent" in x]:
 
         extent_diff = np.diff(extent, 1)
 
+        local_minima_indices = np.where((extent_diff[:-1] < 0) & (extent_diff[1:] > 0))[0] + 1
+        local_minima_values = extent[local_minima_indices]
+
+        local_maxima_indices = np.where((extent_diff[:-1] > 0) & (extent_diff[1:] < 0))[0] + 1
+        local_maxima_values = extent[local_maxima_indices]
+
         m, b = np.polyfit(year, extent, 1)
         bfl_values = m*year + b
+
+        minima_below_bfl_indices = [i for i in local_minima_indices if extent[i] < bfl_values[i]]
+        minima_below_bfl_values = extent[minima_below_bfl_indices]
+        
+        maxima_above_bfl_indices = [i for i in local_maxima_indices if extent[i] > bfl_values[i]]
+        maxima_above_bfl_values = extent[maxima_above_bfl_indices]
 
         # ENSO Data
 
@@ -72,12 +96,34 @@ for sheet in [x for x in df_sea_ice.keys() if "Extent" in x]:
         curr_corr = round(extent.corr(df_soi[month], method='pearson'), 3)
         corr_values.append(curr_corr)
 
-    print(f"\n\033[0;31m{sector.ljust(10)}\t\033[0;33mCorr\033[0m")
+        curr_minima_corr = round(local_minima_values.corr(df_soi[month][local_minima_indices], method='pearson'), 3)
+        minima_corr_values.append(curr_minima_corr)
+
+        curr_minima_below_bfl_corr = round(minima_below_bfl_values.corr(df_soi[month][minima_below_bfl_indices], method='pearson'), 3)
+        minima_below_bfl_corr_values.append(curr_minima_below_bfl_corr)
+
+        curr_maxima_corr = round(local_maxima_values.corr(df_soi[month][local_maxima_indices], method='pearson'), 3)
+        maxima_corr_values.append(curr_maxima_corr)
+
+        curr_maxima_above_bfl_corr = round(maxima_above_bfl_values.corr(df_soi[month][maxima_above_bfl_indices], method='pearson'), 3)
+        maxima_above_bfl_corr_values.append(curr_maxima_above_bfl_corr)
+
+    print(f"\n\033[0;31m{sector.ljust(10)}\t\033[0;33mCorr\tMin\tMinBB\tMax\tMaxAB\033[0m")
     for i in range(len(month_list)):
         
         print(month_list[i].ljust(10), end='\t')
-        print(("\033[0;32m" if abs(corr_values[i]) > pearson_threshold else "") + str(corr_values[i]) + "\033[0m", end='\n')
+        print(("\033[0;32m" if abs(corr_values[i]) > pearson_threshold else "") + str(corr_values[i]) + "\033[0m", end='\t')
+        
+        print(("\033[0;32m" if abs(minima_corr_values[i]) > pearson_threshold else "") + str(minima_corr_values[i]) + "\033[0m", end='\t')
+        print(("\033[0;32m" if abs(minima_below_bfl_corr_values[i]) > pearson_threshold else "") + str(minima_below_bfl_corr_values[i]) + "\033[0m", end='\t')
+        
+        print(("\033[0;32m" if abs(maxima_corr_values[i]) > pearson_threshold else "") + str(maxima_corr_values[i]) + "\033[0m", end='\t')
+        print(("\033[0;32m" if abs(maxima_above_bfl_corr_values[i]) > pearson_threshold else "") + str(maxima_above_bfl_corr_values[i]) + "\033[0m", end='\n')
 
     corr_values.clear()
+    minima_corr_values.clear()
+    minima_below_bfl_corr_values.clear()
+    maxima_corr_values.clear()
+    maxima_above_bfl_corr_values.clear()
 
     # plot_graph(sector, year, extent, bfl_values)
