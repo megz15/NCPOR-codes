e1e5096c267c5b09faa34872cb80b698d88465a4
megz15 <meghraj.g16@gmail.com>
2024-06-04 13:42:32 +0530

    add: extent butterworth filter

---
+++ lowpass_filter/enso_butterworth.py
@@ -0,0 +1,36 @@
+import scipy.signal as signal
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# Load data
+
+excel_path = "../../data/S_Sea_Ice_Index_Regional_Monthly_Data_G02135_v3.0.xlsx"
+
+df_sea_ice = pd.read_excel(excel_path, "Ross-Extent-km^2").head(-1).tail(-3).reset_index()
+year = pd.Series(df_sea_ice["Unnamed: 0"]).astype(int)
+extent = pd.Series(df_sea_ice["January"]).astype(float)
+extent = extent.interpolate(method='linear')
+
+N  = 2    # Filter order
+Wn = 0.6  # Cutoff frequency
+B, A = signal.butter(N, Wn, output='ba')
+
+extentf = signal.filtfilt(B,A, extent)
+
+# Make plots
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+plt.plot(year, extent, 'b-')
+plt.plot(year, extentf, 'r-',linewidth=2)
+plt.legend(['Original','Filtered'])
+plt.xlabel("Year")
+plt.title("Sea Ice Extent")
+ax1.axes.get_xaxis().set_visible(False)
+
+ax1 = fig.add_subplot(212)
+plt.plot(year,extent-extentf, 'b-')
+plt.xlabel("Year")
+plt.legend(['Residuals'])
+
+plt.show()
