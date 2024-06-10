366b5d5f2f805cfc4fce2cedec2d9769aaf30a49
megz15 <meghraj.g16@gmail.com>
2024-06-10 14:51:34 +0530

    feat: extent nc corr for all regions

--- era5/radiation/str.py
+++ era5/radiation/str.py
@@ -20,7 +20,6 @@ regions = {
     'Pacific': (90, 160)
 }
 
-region = list(regions.keys())[4]
 rel_path = "../../data/"
 
 year_list = list(range(1979, 2024))
@@ -38,59 +37,61 @@ month_days = {
     "November": 30,
     "December": 31
 }
+for region in list(regions.keys()):
+    # Sea Ice Index Data
+    excel_path = rel_path + "S_Sea_Ice_Index_Regional_Daily_Data_G02135_v3.0.xlsx"
+    df_sea_ice = pd.read_excel(excel_path, sheet_name = region + "-Extent-km^2")
+    df_sea_ice.drop(df_sea_ice.columns[[-1, 0, 1, 2]],axis=1,inplace=True)
 
-# Sea Ice Index Data
-excel_path = rel_path + "S_Sea_Ice_Index_Regional_Daily_Data_G02135_v3.0.xlsx"
-df_sea_ice = pd.read_excel(excel_path, sheet_name = region + "-Extent-km^2")
-df_sea_ice.drop(df_sea_ice.columns[[-1, 0, 1, 2]],axis=1,inplace=True)
+    extent_values = []
+    for year in year_list:
+        yearly_data = df_sea_ice[year].values
+        if not is_leap_year(year):
+            # Drop the 59th day (February 29) for non-leap years
+            yearly_data = np.concatenate((yearly_data[:59], yearly_data[60:]))
+        extent_values.extend(yearly_data)
 
-extent_values = []
-for year in year_list:
-    yearly_data = df_sea_ice[year].values
-    if not is_leap_year(year):
-        # Drop the 59th day (February 29) for non-leap years
-        yearly_data = np.concatenate((yearly_data[:59], yearly_data[60:]))
-    extent_values.extend(yearly_data)
+    extent = pd.Series(extent_values).astype(float)
+    extent.interpolate(method='linear', inplace=True)
+    extent = pd.Series(signal.filtfilt(B,A, extent))
 
-extent = pd.Series(extent_values).astype(float)
-extent.interpolate(method='linear', inplace=True)
-extent = pd.Series(signal.filtfilt(B,A, extent))
+    date_range = pd.date_range(start='1979-01-01', end='2023-12-31', freq='D')
+    df_daily = pd.DataFrame(extent, columns=['value'])
+    df_daily.set_index(date_range, inplace=True)
 
-date_range = pd.date_range(start='1979-01-01', end='2023-12-31', freq='D')
-df_daily = pd.DataFrame(extent, columns=['value'])
-df_daily.set_index(date_range, inplace=True)
+    df_monthly = df_daily.resample('M').mean()
+    df_monthly['Year'] = df_monthly.index.year
+    df_monthly['Month'] = df_monthly.index.month
 
-df_monthly = df_daily.resample('M').mean()
-df_monthly['Year'] = df_monthly.index.year
-df_monthly['Month'] = df_monthly.index.month
+    df_extent_resampled_monthly = df_monthly.pivot(index='Year', columns='Month', values='value')
+    # df_extent_resampled_monthly.reset_index(drop=True, inplace=True)
 
-df_extent_resampled_monthly = df_monthly.pivot(index='Year', columns='Month', values='value')
-# df_extent_resampled_monthly.reset_index(drop=True, inplace=True)
+    # NetCDF Data
+    nv_path = rel_path + 'netcdf/rad/radiation_1979-2023_monthly_seaice.nc'
 
-# NetCDF Data
-nv_path = rel_path + 'netcdf/rad/radiation_1979-2023_monthly_seaice.nc'
+    with xr.open_dataset(nv_path) as ds:
+        resampled_ds = ds['str'].sel(
+            longitude = slice(
+                min(regions[region]), max(regions[region])
+            )).mean(dim=['latitude', 'longitude'])
+        
+        resampled_df = resampled_ds.to_dataframe().reset_index()
+        
+        resampled_df['Month'] = resampled_df['time'].dt.month
+        resampled_df['Year'] = resampled_df['time'].dt.year
+        
+        df_nc_resampled_monthly = resampled_df.pivot(index='Year', columns='Month', values='str')
+        # df_nc_resampled_monthly.reset_index(drop=True, inplace=True)
 
-with xr.open_dataset(nv_path) as ds:
-    resampled_ds = ds['str'].sel(
-        longitude = slice(
-            min(regions[region]), max(regions[region])
-        )).mean(dim=['latitude', 'longitude'])
-    
-    resampled_df = resampled_ds.to_dataframe().reset_index()
-    
-    resampled_df['Month'] = resampled_df['time'].dt.month
-    resampled_df['Year'] = resampled_df['time'].dt.year
-    
-    df_nc_resampled_monthly = resampled_df.pivot(index='Year', columns='Month', values='str')
-    # df_nc_resampled_monthly.reset_index(drop=True, inplace=True)
+    print(f"\n\033[0;31m{region.ljust(10)}\t\033[0;33mCorr\tp-Value\033[0m")
+    for month in list(range(len(month_days))):
 
-print(f"\n\033[0;31m{region.ljust(10)}\t\033[0;33mCorr\033[0m")
-for month in list(range(len(month_days))):
+        # curr_corr = round(df_pivot[month].corr(df_soi[month], method="pearson"), 3)
+        curr_corr, p_value = pearsonr(df_extent_resampled_monthly[month + 1], df_nc_resampled_monthly[month + 1])
+        curr_corr = round(curr_corr, 3)
+        p_value = round(p_value, 3)
 
-    # curr_corr = round(df_pivot[month].corr(df_soi[month], method="pearson"), 3)
-    curr_corr, p_value = pearsonr(df_extent_resampled_monthly[month + 1], df_nc_resampled_monthly[month + 1])
-    curr_corr = round(curr_corr, 3)
-
-    print(list(month_days.keys())[month].ljust(10), end='\t')
-    # print(("\033[0;32m" if abs(curr_corr) > pearson_threshold else "") + str(curr_corr) + "\033[0m")
-    print(("\033[0;32m" if p_value < 0.05 else "") + str(curr_corr) + "\033[0m")
+        print(list(month_days.keys())[month].ljust(10), end='\t')
+        # print(("\033[0;32m" if abs(curr_corr) > pearson_threshold else "") + str(curr_corr) + "\033[0m")
+        print(("\033[0;32m" if p_value < 0.05 else "") + str(curr_corr) + "\033[0m", end='\t')
+        print(p_value)
