f3c89cbc0fd2808668dafbf766b26c08cb6349b9
megz15 <meghraj.g16@gmail.com>
2024-06-10 15:37:13 +0530

    feat: extent nc corr plots

--- era5/radiation/str.py
+++ era5/radiation/str.py
@@ -3,15 +3,17 @@ import pandas as pd
 import numpy as np
 import scipy.signal as signal
 from scipy.stats import pearsonr
+import matplotlib.pyplot as plt
 
-def is_leap_year(year): return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+def is_leap_year(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 # Butterworth Filter params
 bw_order  = 1     # Filter order
 bw_cfreq  = 0.4   # Cutoff frequency
 B, A = signal.butter(bw_order, bw_cfreq, btype='lowpass', analog=False)
 
-# longitude ranges
+# Longitude ranges
 regions = {
     'Ross': (160, -130),
     'Bell-Amundsen': (-130, -60),
@@ -38,6 +40,7 @@ month_days = {
     "December": 31
 }
 for region in list(regions.keys()):
+    
     # Sea Ice Index Data
     excel_path = rel_path + "S_Sea_Ice_Index_Regional_Daily_Data_G02135_v3.0.xlsx"
     df_sea_ice = pd.read_excel(excel_path, sheet_name = region + "-Extent-km^2")
@@ -64,7 +67,6 @@ for region in list(regions.keys()):
     df_monthly['Month'] = df_monthly.index.month
 
     df_extent_resampled_monthly = df_monthly.pivot(index='Year', columns='Month', values='value')
-    # df_extent_resampled_monthly.reset_index(drop=True, inplace=True)
 
     # NetCDF Data
     nv_path = rel_path + 'netcdf/rad/radiation_1979-2023_monthly_seaice.nc'
@@ -81,17 +83,28 @@ for region in list(regions.keys()):
         resampled_df['Year'] = resampled_df['time'].dt.year
         
         df_nc_resampled_monthly = resampled_df.pivot(index='Year', columns='Month', values='str')
-        # df_nc_resampled_monthly.reset_index(drop=True, inplace=True)
+
+    fig, axes = plt.subplots(3, 4)
+    axes = axes.flatten()
 
     print(f"\n\033[0;31m{region.ljust(10)}\t\033[0;33mCorr\tp-Value\033[0m")
     for month in list(range(len(month_days))):
 
-        # curr_corr = round(df_pivot[month].corr(df_soi[month], method="pearson"), 3)
         curr_corr, p_value = pearsonr(df_extent_resampled_monthly[month + 1], df_nc_resampled_monthly[month + 1])
         curr_corr = round(curr_corr, 3)
         p_value = round(p_value, 3)
 
         print(list(month_days.keys())[month].ljust(10), end='\t')
-        # print(("\033[0;32m" if abs(curr_corr) > pearson_threshold else "") + str(curr_corr) + "\033[0m")
         print(("\033[0;32m" if p_value < 0.05 else "") + str(curr_corr) + "\033[0m", end='\t')
         print(p_value)
+
+        # Plotting
+        ax = axes[month]
+        ax.plot(df_extent_resampled_monthly.index, df_extent_resampled_monthly[month + 1], label='Sea Ice Extent', color='b')
+        ax.plot(df_nc_resampled_monthly.index, df_nc_resampled_monthly[month + 1], label='Surface Radiation', color='r')
+        ax.set_title(f"{region} - {list(month_days.keys())[month]}")
+        ax.legend()
+        ax.grid(True)
+
+    # plt.tight_layout()
+    plt.show()
