1fcf416048bc873c7b328c14eea9fdc549a7a73e
megz15 <meghraj.g16@gmail.com>
2024-06-12 16:01:59 +0530

    feat: plots

--- era5/radiation/monthly/ssr.py
+++ era5/radiation/monthly/ssr.py
@@ -1,6 +1,9 @@
+from matplotlib import pyplot as plt
+import numpy as np
 import xarray as xr
 import pandas as pd
 from scipy.stats import pearsonr
+import scipy.signal as signal
 
 # Longitude and Latitude ranges
 regions = {
@@ -61,6 +64,9 @@ for region in list(regions.keys()):
         
         df_nc_resampled_monthly = resampled_df.pivot(index='Year', columns='Month', values='ssr')
 
+    fig, axes = plt.subplots(3, 4, sharex=True, sharey=True)
+    axes = axes.flatten()
+
     print(f"\n\033[0;31m{region.ljust(10)}\t\033[0;33mCorr\tp-Value\033[0m")
     for month in month_list:
 
@@ -73,6 +79,10 @@ for region in list(regions.keys()):
         extent = extent.transform("rank")
         df_nc_resampled_monthly = df_nc_resampled_monthly.transform("rank")
 
+        # Best fit
+        coefficients = np.polyfit(year, extent, 5)
+        bfl_values = np.polyval(coefficients, year)
+
         curr_corr, p_value = pearsonr(extent, df_nc_resampled_monthly[month_list.index(month) + 1])
         curr_corr = round(curr_corr, 3)
         p_value = round(p_value, 3)
@@ -80,3 +90,34 @@ for region in list(regions.keys()):
         print(month.ljust(10), end='\t')
         print(("\033[0;32m" if p_value < 0.05 else "") + str(curr_corr) + "\033[0m", end='\t')
         print(p_value)
+
+        # Plotting
+        ax = axes[month_list.index(month)]
+        ax.plot(year, extent, label='Sea Ice Extent', color='b')
+        ax.plot(year, bfl_values, label='Extent Best Fit', color='g')
+
+        # Identifying minimas and maximas
+        residuals = extent - bfl_values
+        minima_indices = signal.argrelextrema(residuals.values, np.less)[0]
+        maxima_indices = signal.argrelextrema(residuals.values, np.greater)[0]
+
+        minimas = [(i, residuals.values[i]) for i in minima_indices if residuals.values[i] < 0]
+        maximas = [(i, residuals.values[i]) for i in maxima_indices if residuals.values[i] > 0]
+
+        minimas_sorted = sorted(minimas, key=lambda x: x[1])
+        maximas_sorted = sorted(maximas, key=lambda x: x[1], reverse=True)
+
+        for idx, (i, _) in enumerate(minimas_sorted):
+            ax.annotate(f'A{idx+1}', xy=(year[i], extent.iloc[i]), 
+                        xytext=(year[i], extent.iloc[i] - 0.1))
+
+        for idx, (i, _) in enumerate(maximas_sorted):
+            ax.annotate(f'B{idx+1}', xy=(year[i], extent.iloc[i]), 
+                        xytext=(year[i], extent.iloc[i] + 0.1))
+
+        ax.plot(df_nc_resampled_monthly.index, df_nc_resampled_monthly[month_list.index(month) + 1], label='Surface Radiation', color='r')
+        ax.set_title(f"{region} - {month}")
+        ax.legend()
+        ax.grid(True)
+
+    plt.show()
