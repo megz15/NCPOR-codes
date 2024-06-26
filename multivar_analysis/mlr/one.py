0bdccc7f4c23f231f678a46be6dc0db083ef8a5c
megz15 <meghraj.g16@gmail.com>
2024-06-26 11:05:26 +0530

    feat: add ssr to dv

--- multivar_analysis/mlr/one.py
+++ multivar_analysis/mlr/one.py
@@ -1,8 +1,8 @@
-from scipy.stats import pearsonr
 import scipy.signal as signal
 import pandas as pd
-import statsmodels.api as sm
 from sklearn.preprocessing import StandardScaler
+from sklearn import linear_model
+import xarray as xr
 
 # Parameters
 
@@ -13,6 +13,14 @@ B, A = signal.butter(bw_order, bw_cfreq, btype='low', analog=False)
 
 sectors = {"Bell-Amundsen": None, "Indian": None, "Pacific": None, "Ross": None, "Weddell": None}
 rel_path = "../../data/"
+
+regions = {
+    'Ross': {'lon': (160, -130), 'lat': (-50, -72)},
+    'Bell-Amundsen': {'lon': (-130, -60), 'lat': (-50, -72)},
+    'Weddell': {'lon': (-60, 20), 'lat': (-50, -72)},
+    'Indian': {'lon': (20, 90), 'lat': (-50, -72)},
+    'Pacific': {'lon': (90, 160), 'lat': (-50, -72)}
+}
 
 month_list = [
     'January',
@@ -43,17 +51,38 @@ dataset_path = rel_path + "ersst.v5.pdo.dat"
 df_pdo = pd.read_csv(dataset_path, delim_whitespace=True, names=columns)
 df_pdo = df_pdo[df_pdo['Year']>=1979].head(-1).drop('Year', axis=1).reset_index(drop=True)
 
-# Dependent Variable (DV)
 
-# Sea Ice Index Data
-dataset_path = rel_path + "S_Sea_Ice_Index_Regional_Monthly_Data_G02135_v3.0.xlsx"
 for sector in list(sectors.keys()):
+
+    # SSR Data
+    dataset_path = rel_path + 'netcdf/rad/radiation_1979-2023_monthly_seaice.nc'
+    with xr.open_dataset(dataset_path) as ds:
+        resampled_ds = ds['ssr'].sel(
+            longitude = slice(
+                min(regions[sector]['lon']), max(regions[sector]['lon'])
+            ), latitude = slice(
+                max(regions[sector]['lat']), min(regions[sector]['lat'])
+            )).mean(dim=['latitude', 'longitude'])
+        
+        df_ssr = resampled_ds.to_dataframe().reset_index()
+        
+        df_ssr['Year'] = df_ssr['time'].dt.year
+        df_ssr['Month'] = df_ssr['time'].dt.strftime('%B')
+        
+        df_ssr = df_ssr.pivot(index='Year', columns='Month', values='ssr')
+        df_ssr.reset_index(inplace=True)
+
+    # Dependent Variable (DV)
+
+    # Sea Ice Index Data
+    dataset_path = rel_path + "S_Sea_Ice_Index_Regional_Monthly_Data_G02135_v3.0.xlsx"
+
     sectors[sector] = pd.read_excel(dataset_path, sheet_name = sector + "-Extent-km^2")[month_list].head(-1).tail(-3).reset_index(drop=True).apply(pd.to_numeric)
     sectors[sector] = sectors[sector].interpolate(method='linear')
 
 dv = {}
 for month in month_list:
-    dv[month] = pd.DataFrame({"ENSO": df_soi[month], "PDO": df_pdo[month]})
+    dv[month] = pd.DataFrame({"ENSO": df_soi[month], "PDO": df_pdo[month], "SSR": df_ssr[month]})
 
 # Standardizing dv values
 scaler = StandardScaler()
@@ -66,11 +95,11 @@ def perform_mlr(iv, dv):
     for month in iv.columns:
         x = dv[month]
         y = iv[month]
-        
-        x = sm.add_constant(x)
-        model = sm.OLS(y, x).fit()
 
-        coeffs[month] = model.params
+        model = linear_model.LinearRegression()
+        model.fit(x, y)
+
+        coeffs[month] = {'const': model.intercept_, 'ENSO': model.coef_[0], 'PDO': model.coef_[1], 'SSR': model.coef_[2]}
     return coeffs
 
 coeffs = {}
@@ -80,5 +109,5 @@ for region, iv in sectors.items():
 for region, coeffs in coeffs.items():
     print(f"\nEquations for {region} region:")
     for month, coeff in coeffs.items():
-        equation = f"Sea Ice Extent = {coeff['const']} + {coeff['ENSO']}*(ENSO) + {coeff['PDO']}*(PDO)"
+        equation = f"Sea Ice Extent = {coeff['const']} + {coeff['ENSO']}*(ENSO) + {coeff['PDO']}*(PDO) + {coeff['SSR']}*(SSR)"
         print(f"{month}: {equation}")
