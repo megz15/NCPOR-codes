530bb627d90d233631a89149c2dafd7a3039f2d5
megz15 <meghraj.g16@gmail.com>
2024-07-01 12:01:08 +0530

    feat: add flag to calc intercept

--- multivar_analysis/mlr/one.py
+++ multivar_analysis/mlr/one.py
@@ -3,9 +3,8 @@ from sklearn.linear_model import LinearRegression
 import pandas as pd
 
 # Parameters
-
 poly_deg = 1
-
+calc_intercept = False
 rel_path = "../../data/"
 
 sectors = {
@@ -17,24 +16,13 @@ sectors = {
 }
 
 month_list = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
 ]
 
 columns = ['Year'] + month_list
 
 # Independent Variables (IVs)
-
 df_soi = pd.read_pickle('pickles/soi.pkl')
 df_pdo = pd.read_pickle('pickles/pdo.pkl')
 df_iod = pd.read_pickle('pickles/iod.pkl')
@@ -42,52 +30,49 @@ df_ssr = pd.read_pickle('pickles/ssr.pkl')
 df_str = pd.read_pickle('pickles/str.pkl')
 
 # Dependent Variable (DV)
-
 df_sie = pd.read_pickle('pickles/sie.pkl')
 
-dv = {}
+iv = {}
 for month in month_list:
-    dv[month] = pd.DataFrame({"ENSO": df_soi[month], "PDO": df_pdo[month], "IOD": df_iod[month]})
+    iv[month] = pd.DataFrame({"ENSO": df_soi[month], "PDO": df_pdo[month], "IOD": df_iod[month]})
 
-# Standardizing dv values
+# Standardizing iv values
 scaler = StandardScaler()
 
-for month, df in dv.items():
-    dv[month] = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+for month, df in iv.items():
+    iv[month] = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
 
-def perform_mlr(iv, dv, region_dv):
+def perform_mlr(dv, iv, region_iv):
     coeffs = {}
     for month in month_list:
-        for i in list(region_dv.keys()):
-            dv[month][i] = region_dv[i][month]
+        for i in list(region_iv.keys()):
+            iv[month][i] = region_iv[i][month]
 
-        x = dv[month]
-        y = iv[month]
+        x = iv[month]
+        y = dv[month]
 
-        poly = PolynomialFeatures(degree=poly_deg)
+        poly = PolynomialFeatures(degree = poly_deg, include_bias = calc_intercept)
         x_poly = poly.fit_transform(x)
 
-        model = LinearRegression()
+        model = LinearRegression(fit_intercept = False)
         model.fit(x_poly, y)
 
         coeffs[month] = dict(zip(poly.get_feature_names_out().tolist(), model.coef_.tolist()))
-        del coeffs[month]['1']
-        coeffs[month] = {'const':model.intercept_, **coeffs[month]}
     return coeffs
 
 coeffs = {}
 for sector in list(sectors.keys()):
-    df_sie_r = df_sie[df_sie['Sector'] == sector].reset_index(drop=True)
+    df_sie_r = df_sie[df_sie['Sector'] == sector]
     df_ssr_r = df_ssr[df_ssr['Sector'] == sector].reset_index(drop=True)
     df_str_r = df_str[df_str['Sector'] == sector].reset_index(drop=True)
 
     df_sie_r = df_sie_r.drop(columns = ['Sector', 'Year']).reset_index(drop=True)
 
-    coeffs[sector] = perform_mlr(df_sie_r, dv, {
+    coeffs[sector] = perform_mlr(df_sie_r, iv, {
         'SSR': df_ssr_r, 'STR': df_str_r
     })
 
 for sector, coeffs in coeffs.items():
     print(f"\n\nEquations for {sector} sector:")
     for month, coeff in coeffs.items():
-        print(f'\n{month}:\n' + ' + '.join([f'{coeff[x]}*({x})' if x!='const' else str(coeff[x]) for x in coeff]))
+        print(f'\n{month}:\n' + ' + '.join([f'{coeff[x]}*({x})' for x in coeff]))
