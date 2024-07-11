523f9e4f52d876b5204ffaffdf8f0ebe8ab678f1
megz15 <meghraj.g16@gmail.com>
2024-07-11 10:53:44 +0530

    feat: add other datasets, flatten and standardize

--- multivar_analysis/compare_iv/time_series.py
+++ multivar_analysis/compare_iv/time_series.py
@@ -1,25 +1,25 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 def flatten(df, sector = None):
 
     if sector: df = df.drop(columns=["Sector"])
 
-    year = 'Year'
-
-    try:
-        df_flat = pd.melt(df, id_vars=[year], var_name='Month', value_name='Value')
-    except KeyError as e:
-        year = 'year'
-        df_flat = pd.melt(df, id_vars=[year], var_name='Month', value_name='Value')
+    df_flat = pd.melt(df, id_vars=['Year'], var_name='Month', value_name='Value')
 
     df_flat['Month'] = df_flat['Month'].map(months)
-    df_flat['Date'] = pd.to_datetime(df_flat[year].astype(str) + '-' + df_flat['Month'] + '-01')
+    df_flat['Date'] = pd.to_datetime(df_flat['Year'].astype(str) + '-' + df_flat['Month'] + '-01')
     
-    df_flat.drop(columns=[year, 'Month'], inplace=True)
+    df_flat.drop(columns=['Year', 'Month'], inplace=True)
     df_flat.sort_values(by='Date', inplace=True)
     df_flat.reset_index(drop = True, inplace=True)
     
     return df_flat
+
+def standardize(df):
+    scaler = StandardScaler()
+    df['Value'] = scaler.fit_transform(df[['Value']])
+    return df
 
 sectors = {
     'Ross': {'lon': (160, -130), 'lat': (-50, -72)},
@@ -44,32 +44,42 @@ df_iod = pd.read_pickle('pickles/iod.pkl')
 df_ssr = pd.read_pickle('pickles/ssr.pkl')
 df_str = pd.read_pickle('pickles/str.pkl')
 
-df_u10 = pd.read_pickle('pickles/u10.pkl')
-df_v10 = pd.read_pickle('pickles/v10.pkl')
+# df_u10 = pd.read_pickle('pickles/u10.pkl')
+# df_v10 = pd.read_pickle('pickles/v10.pkl')
 
-df_t2m = pd.read_pickle('pickles/t2m.pkl')
+# df_t2m = pd.read_pickle('pickles/t2m.pkl')
 
 # Others
 df_slhf = pd.ExcelFile('other_data/monthly_slhf_value_filtered.xlsx')
 df_sshf = pd.ExcelFile('other_data/monthly_sshf_value_filtered.xlsx')
 
+df_u10 = pd.ExcelFile('other_data/monthly_u10_value_filtered.xlsx')
+df_v10 = pd.ExcelFile('other_data/monthly_v10_value_filtered.xlsx')
+
+df_t2m = pd.ExcelFile('other_data/monthly_t2m_value_filtered.xlsx')
+
 # Dependent Variable (DV)
 df_sie = pd.read_pickle('pickles/sie.pkl')
 
-df_soi_flat = flatten(df_soi)
-df_pdo_flat = flatten(df_pdo)
-df_iod_flat = flatten(df_iod)
+df_soi_flat = standardize(flatten(df_soi))
+df_pdo_flat = standardize(flatten(df_pdo))
+df_iod_flat = standardize(flatten(df_iod))
 
 for sector in sectors:
 
-    df_ssr_flat = flatten(df_ssr[ df_ssr["Sector"] == sector ], sector)
-    df_str_flat = flatten(df_str[ df_str["Sector"] == sector ], sector)
+    df_ssr_flat = standardize(flatten(df_ssr[ df_ssr["Sector"] == sector ], sector))
+    df_str_flat = standardize(flatten(df_str[ df_str["Sector"] == sector ], sector))
 
-    df_u10_flat = flatten(df_u10[ df_u10["Sector"] == sector ], sector)
-    df_v10_flat = flatten(df_v10[ df_v10["Sector"] == sector ], sector)
+    # df_u10_flat = standardize(flatten(df_u10[ df_u10["Sector"] == sector ], sector))
+    # df_v10_flat = standardize(flatten(df_v10[ df_v10["Sector"] == sector ], sector))
 
-    df_t2m_flat = flatten(df_t2m[ df_t2m["Sector"] == sector ], sector)
-    df_t2m_flat['Value'] = df_t2m_flat['Value'].subtract(273.15)
+    # df_t2m_flat = standardize(flatten(df_t2m[ df_t2m["Sector"] == sector ], sector))
+    # df_t2m_flat['Value'] = df_t2m_flat['Value'].subtract(273.15)
 
-    df_slhf_flat = flatten(pd.read_excel(df_slhf, f'{sector} Region'))
-    df_sshf_flat = flatten(pd.read_excel(df_sshf, f'{sector} Region'))
+    df_slhf_flat = standardize(flatten(pd.read_excel(df_slhf, f'{sector} Region')))
+    df_sshf_flat = standardize(flatten(pd.read_excel(df_sshf, f'{sector} Region')))
+
+    df_u10_flat = standardize(flatten(pd.read_excel(df_u10, f'{sector} Region')))
+    df_v10_flat = standardize(flatten(pd.read_excel(df_v10, f'{sector} Region')))
+
+    df_t2m_flat = standardize(flatten(pd.read_excel(df_t2m, f'{sector} Region')))
