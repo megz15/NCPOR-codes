ae92e6817cd444bef8c7511547ada88bbd7924bf
megz15 <meghraj.g16@gmail.com>
2024-07-12 23:17:34 +0530

    feat: separate remote and reanalysis

--- multivar_analysis/mlr/ols_monthly_percentages.py
+++ multivar_analysis/mlr/ols_monthly_percentages.py
@@ -1,35 +1,24 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 import pandas as pd
-
-def highlight_if_significant(flag, threshold = 0.5):
-    return '\033[92m' if flag > threshold else '\033[0m'
+import numpy as np
 
 def df_transform(df, method = "log"):
     return df.transform(method)
 
-def perform_mlr(dv, iv, region_iv):
+def perform_mlr(dv, iv):
     models = {}
-
-    # Standardizing iv values
     scaler = StandardScaler()
 
     for month in month_list:
-        for region in region_iv:
-            iv[month][region] = region_iv[region][month]
-
         iv[month] = pd.DataFrame(scaler.fit_transform(iv[month]), columns=iv[month].columns)
-
-        x = iv[month]
-        y = dv[month]
+        x, y = iv[month], dv[month]
 
         model = LinearRegression()
         model.fit(x, y)
-
         models[month] = model
-    return models
 
-years = range(1979, 2024)
+    return models
 
 sectors = ['Ross', 'Bell-Amundsen', 'Weddell', 'Indian', 'Pacific']
 
@@ -58,17 +47,23 @@ df_t2m = pd.ExcelFile('other_data/monthly_t2m_value_filtered.xlsx')
 df_sie = pd.read_pickle('pickles/sie.pkl')
 df_sie[month_list] = df_transform(df_sie[month_list])
 
-iv = {}
+remote_iv = {}
+reanalysis_iv = {}
+
 for month in month_list:
-    iv[month] = pd.DataFrame({
+    remote_iv[month] = pd.DataFrame({
         'ENSO': df_soi[month], 'PDO': df_pdo[month], 'IOD': df_iod[month],
     })
 
-models = {}
+remote_models = {}
+reanalysis_models = {}
 
 for sector in sectors:
     df_sie_r = df_sie[df_sie['Sector'] == sector]
-    
+
+    remote_models[sector] = perform_mlr(df_sie_r, remote_iv)
+
+    # Reanalysis data
     df_ssr_r = df_ssr[df_ssr['Sector'] == sector].reset_index(drop=True)
     df_str_r = df_str[df_str['Sector'] == sector].reset_index(drop=True)
     
@@ -79,27 +74,35 @@ for sector in sectors:
     df_v10_r = pd.read_excel(df_v10, f'{sector} Region')
 
     df_t2m_r = pd.read_excel(df_t2m, f'{sector} Region')
+    df_t2m_r = df_t2m_r[(df_t2m_r['Year'] >= 1979) & (df_t2m_r['Year'] <= 2023)]
 
     df_sie_r = df_sie_r.drop(columns = ['Sector', 'Year']).reset_index(drop=True)
 
-    models[sector] = perform_mlr(df_sie_r, iv, {
-        'SSR': df_ssr_r,
-        'STR': df_str_r,
-        'SLHF': df_slhf_r,
-        'SSHF': df_sshf_r,
-        'U10': df_u10_r,
-        'V10': df_v10_r,
-        'T2M': df_t2m_r,
+    for month in month_list:
+        reanalysis_iv[month] = pd.DataFrame({
+        'SSR': df_ssr_r[month],
+        'STR': df_str_r[month],
+        'SLHF': df_slhf_r[month],
+        'SSHF': df_sshf_r[month],
+        'U10': df_u10_r[month],
+        'V10': df_v10_r[month],
+        'T2M': df_t2m_r[month],
     })
 
-for sector, sector_models in models.items():
+    reanalysis_models[sector] = perform_mlr(df_sie_r, reanalysis_iv)
+
+for sector, remote_models, reanalysis_models in zip(sectors, remote_models.values(), reanalysis_models.values()):
     print(f'\n\033[105mEquations for {sector} sector:\033[0m')
-    for month, model in sector_models.items():
-        coefficients = model.coef_
-        feature_names = model.feature_names_in_
+    
+    for month, remote_model, reanalysis_model in zip(month_list, remote_models.values(), reanalysis_models.values()):
 
-        total_abs_coefs = sum(abs(coeff) for coeff in coefficients)
-        contributions = {name: round(abs(coeff) / total_abs_coefs * 100, 3) for name, coeff in zip(feature_names, coefficients)}
+        print("\033[93mMonth - " + month)
+        for kind, model in zip(("Remote", "Reanalysis"), (remote_model, reanalysis_model)):
+            coefficients = model.coef_
+            feature_names = model.feature_names_in_
 
-        print("\033[93m" + month.ljust(10) + '\t\033[95m' + '\t'.join(contributions.keys()) + '\033[0m')
-        print("Contribution\t" + '%\t'.join(map(str, contributions.values())) + '%')
+            total_abs_coefs = np.sum(np.abs(coefficients))
+            contributions = {name: round(np.abs(coeff) / total_abs_coefs * 100, 3) for name, coeff in zip(feature_names, coefficients)}
+
+            print(f'\033[95m{kind.ljust(10)}\t' + '\t'.join(contributions.keys()) + '\033[0m')
+            print("Contributions\t" + '%\t'.join(map(str, contributions.values())) + '%')
